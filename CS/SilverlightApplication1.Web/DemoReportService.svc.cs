b5a93f46927d5281fd977b3c7af2a7fc7370d701
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication1.Web/DemoReportService.svc.cs
@@ -0,0 +1,16 @@
+﻿using System.ServiceModel;
+using DevExpress.Data.Utils.ServiceModel;
+using DevExpress.XtraReports.UI;
+
+namespace SilverlightApplication1.Web {
+    [SilverlightFaultBehavior]
+    public class DemoReportService : DevExpress.XtraReports.Service.ReportService {
+        protected override void FillDataSources(XtraReport report, string reportName, bool isDesignActive) {
+        }
+
+        protected override void SaveReportLayout(string reportName, byte[] layoutData) {
+            throw new FaultException("This method is not implemented." + 
+                                     "Implement the SaveReportLayout method on the server-side, in the report service code-behind.");
+        }
+    }
+}
---
+++ VB/SilverlightApplication1.Web/DemoReportService.svc.vb
@@ -0,0 +1,17 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.ServiceModel
+Imports DevExpress.Data.Utils.ServiceModel
+Imports DevExpress.XtraReports.UI
+
+Namespace SilverlightApplication1.Web
+	<SilverlightFaultBehavior> _
+	Public Class DemoReportService
+		Inherits DevExpress.XtraReports.Service.ReportService
+		Protected Overrides Sub FillDataSources(ByVal report As XtraReport, ByVal reportName As String, ByVal isDesignActive As Boolean)
+		End Sub
+
+		Protected Overrides Sub SaveReportLayout(ByVal reportName As String, ByVal layoutData() As Byte)
+			Throw New FaultException("This method is not implemented." & "Implement the SaveReportLayout method on the server-side, in the report service code-behind.")
+		End Sub
+	End Class
+End Namespace
