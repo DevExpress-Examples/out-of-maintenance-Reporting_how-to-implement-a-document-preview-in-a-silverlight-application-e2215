d9b0c5adddfaba5e5e25c9215bf51391bc3dcf6c
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication1.Web/SampleReport.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+
+namespace SilverlightApplication1.Web {
+    public partial class SampleReport : DevExpress.XtraReports.UI.XtraReport {
+        public SampleReport() {
+            InitializeComponent();
+        }
+
+    }
+}
---
+++ VB/SilverlightApplication1.Web/DemoReportService.svc.vb
@@ -0,0 +1,23 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Linq
+Imports System.Runtime.Serialization
+Imports System.ServiceModel
+Imports System.ServiceModel.Activation
+Imports DevExpress.Data.Utils.ServiceModel
+Imports DevExpress.Xpf.Printing.Service
+Imports DevExpress.XtraReports.Service
+Imports DevExpress.XtraReports.UI
+
+Namespace SilverlightApplication1.Web
+	' NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ReportService1" in code, svc and config file together.
+	<SilverlightFaultBehavior> _
+	Public Class ReportService1
+		Inherits DevExpress.XtraReports.Service.ReportService
+		Protected Overrides Sub FillDataSources(ByVal report As XtraReport, ByVal reportName As String, ByVal isDesignActive As Boolean)
+		End Sub
+		Protected Overrides Sub SaveReportLayout(ByVal reportName As String, ByVal layoutData() As Byte)
+			Throw New FaultException("This method is not implemented." & "Implement the SaveReportLayout method on the server-side, in the report service code-behind.")
+		End Sub
+	End Class
+End Namespace
